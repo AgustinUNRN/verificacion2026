1e9468dfbf333924d23d1d7e8b1684eb999f84ba
agustin <agustin2345678@gmail.com>
2026-03-22 18:22:05 -0300

    agrega manejo de overflow en la suma, pasa el test

--- src/main/Calc.java
+++ src/main/Calc.java
@@ -1,8 +1,21 @@
 package main;
 
 public class Calc {
+    private static final String ERROR_OVERFLOW_MAX = "El resultado supera el máximo valor numérico permitido por el lenguaje.";
+    private static final String ERROR_OVERFLOW_MIN = "El resultado supera el mínimo valor numérico permitido por el lenguaje.";
+
     public int sumar(int a, int b) {
-        return a+b;
+        long resultado = (long) a + (long) b;
+
+        if (resultado > Integer.MAX_VALUE) { // evaluamos que no se produzca un overflow positivo
+            throw new ArithmeticException(
+                    ERROR_OVERFLOW_MAX);
+        }
+        if (resultado < Integer.MIN_VALUE) { // evaluamos que no se produzca un overflow negativo
+            throw new ArithmeticException(
+                    ERROR_OVERFLOW_MIN);
+        }
+        return (int) resultado;
     }
 
 }
